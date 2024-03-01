6743bea13c1fba19b1f230c0d493878ec0c2e394
Marcelo Aranguiz <marceloaranguiz.dev@gmail.com>
2024-02-29 23:27:27 -0300

    Se agrega componente de control de presupuesto

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup>
-  import Presupuesto from './components/Presupuesto.vue'
+  import { ref } from 'vue';
+  import Presupuesto from './components/Presupuesto.vue';
+  import ControlPresupuesto from './components/ControlPresupuesto.vue'
+
+  const presupuesto = ref(0)
+  const disponible = ref(0)
+
+  const definirPresupuesto = (cantidad) => {
+    presupuesto.value = cantidad
+    disponible.value = cantidad
+  }
+
 </script>
 
 <template>
@@ -9,7 +20,13 @@
 
       <div class="contenedor-header contenedor sombra">
         <Presupuesto 
-        
+          v-if="presupuesto === 0"
+          @definir-presupuesto="definirPresupuesto"
+        />
+        <ControlPresupuesto 
+          v-else
+          :presupuesto="presupuesto"
+          :disponible="disponible"
         />
       </div>
       
