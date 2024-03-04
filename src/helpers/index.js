ef110bb981bddb36da9f26a611f0131fe010fff6
Marcelo Aranguiz <marceloaranguiz.dev@gmail.com>
2024-03-04 20:48:38 -0300

    Enlace de Netlify en README

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,7 @@
 export const formatearCantidad = cantidad => {
-    return Number(cantidad).toLocaleString('es-US', {
+    return Number(cantidad).toLocaleString('es-CL', {
         style: 'currency',
-        currency: 'USD'
+        currency: 'CLP'
     })
 }
 
